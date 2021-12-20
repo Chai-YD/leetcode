b6fa007a6075ce4a3c51a5eb3e71c4417d5bae58
chaiyandong <1030333788@qq.com>
2021-12-21 00:12:16 +0800

    REASON: add FindGreatestSumStubArray of return vector

--- 20211211/dynamic_programming/dynamic_programming.cc
+++ 20211211/dynamic_programming/dynamic_programming.cc
@@ -21,3 +21,40 @@ int dynamic_proramming::FindGreatestSumOfSubArray(vector<int> array)
 	}
 	return max_val;
 }
+
+vector<int> dynamic_programming::FindGreatestSumOfSubArray(vector<int> &array)
+{
+	vector<int> vec;
+	if (array.empty()) {
+		return vec;
+	}
+	int max_val = array[0];
+	int cur_val = array[0];
+
+	int ret_left = 0;
+	int ret_right = 0;
+
+	int cur_left = 0;
+	int cur_right = 0;
+	for (int ix = 1; ix < array.size(); ix++) {
+		if (array[ix] > cur_val + array[ix]) {
+			cur_val = array[ix];
+			cur_left = cur_right = ix;
+		}
+		else {
+			cur_val += array[ix];
+			cur_right = ix;
+		}
+		if (cur_val >= max_val) {
+			max_val = cur_val;
+			ret_left = cur_left;
+			ret_right = cur_right;
+		}
+	}
+
+	for(int ix = ret_left; ix <= ret_right; ix++) {
+		vec.push_back(array[ix]);
+	}
+
+	return vec;
+}
--- 20211211/dynamic_programming/dynamic_programming.h
+++ 20211211/dynamic_programming/dynamic_programming.h
@@ -6,4 +6,5 @@ class dynamic_programming
 private:
 public:
 	int FindGreatestSumOfSubArray(vector<int> array);
+	vector<int> FindGreatestSumOfSubArray(vector<int> &array);
 };
