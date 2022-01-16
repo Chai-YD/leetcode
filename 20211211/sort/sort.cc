614ad2bd1ef4154e618ade4e63e21f7dfbb0bb48
chaiyandong <1030333788@qq.com>
2022-01-16 16:41:02 +0800

    REASON: add

--- 20211211/sort/sort.cc
+++ 20211211/sort/sort.cc
@@ -17,8 +17,78 @@ int mod_sort::duplicate(vector<int>& numbers)
 	return -1;
 }
 
+void mod_sort::merge(vector<int> &data, int start, int mid, int end, vector<int> &res)
+{
+	int ix = start; jx = mid + 1;
+	int cur = start;
+	while (ix <= mid && jx <= end) {
+		if (data[ix] > data[jx]) {
+			cnt = (cnt + mid - ix + 1) % 1000000007;
+			res[cnt++] = data[jx++];
+		}
+		else {
+			res[cnt++] = data[ix++];
+		}
+	}
+	while(ix <=mid) {
+		res[cnt++] = data[ix++];
+	}
+	while (jx <=end) {
+		res[cnt++] = data[jx++];
+	}
+	for (int ix = start; ix <= end; ix++) {
+		data[ix] = tmp[ix];
+	}
+}
+
+void mod_sort::mergesort(vector<int> &data, int start, int end, vector<int> &res)
+{
+	if (start < end) {
+		int mid = (start + mid) >> 1;
+		mergesort(data, start, mid);
+		mergesort(data, mid + 1, end);
+		merge(data, start, mid, end);
+	}
+}
+
 int mod_sort::InversePairs(vector<int> data)
 {
+	int cnt = data.size();
+	vector<int> res(n);
+	if (cnt != 0) {
+		mergesort(data, start, 0, cnt - 1, res);
+	}
+	return cnt;
+}
+
+int mod_sort::mergeSort_ex(int left, int right, vector<int>& data, vector<int>& temp)
+{
+	if (left >= right)    // 停止划分
+		return 0;
+	int mid = (left + right) / 2; //取中间
+	int res = mergeSort(left, mid, data, temp) + mergeSort(mid + 1, right, data, temp); //左右划分
+	res %= mod;  //防止溢出
+	int i = left, j = mid + 1;
+	for (int k = left; k <= right; k++)
+		temp[k] = data[k];
+	for (int k = left; k <= right; k++) {
+		if (i == mid + 1)
+			data[k] = temp[j++];
+		else if (j == right + 1 || temp[i] <= temp[j])
+			data[k] = temp[i++];
+		else { //左边比右边大，答案增加
+			data[k] = temp[j++];
+			res += mid - i + 1; // 统计逆序对
+		}
+	}
+	return res % mod;
+}
+
+int mod_sort::InversePairs_ex(vector<int> data)
+{
+	int n = data.size();
+	vector<int> res(n);
+	return mergeSort(0, n - 1, data, res);
 }
 
 vector<int> mod_sort::GetLeastNumbers_Solution(vector<int> input, int k)
--- 20211211/sort/sort.h
+++ 20211211/sort/sort.h
@@ -3,9 +3,18 @@
 class mod_sort 
 {
 private:
+	int cnt = 0;
+	int mod = 1000000007;
 public:
 	int duplicate(vector<int>& numbers);
+
+	void merge(vector<int> &data, int start, int mid, int end);
+	void mergesort(vector<int> &data, int start, int end);
 	int InversePairs(vector<int> data);
+
+	int mergeSort_ex(int left, int right, vector<int>& data, vector<int>& temp)
+	int InversePairs_ex(vector<int> data); ;
+
 	vector<int> GetLeastNumbers_Solution(vector<int> input, int k);
 	void Insert(int num);
 	double GetMedian();
